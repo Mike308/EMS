c0090f53d43b900b88d2c14d5036e507756ab6ab
Majkel44 <m.werner9205@gmail.com>
2016-09-15 21:30:53 +0200

    - added ems controller but is still developing

---
+++ controllers/EMS_Controller.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 2016-09-15
+ * Time: 21:03
+ */
+
+include "../services/EMS_DB.php";
+
+$phase_no = $_GET['phase_no'];
+$cmd = $_GET['cmd'];
+
+$ems_db = new EMS_DB("root","","localhost");
+$ems_db->open_db();
+
+
+
+
+if($cmd==0){
+
+    $result = json_encode($ems_db->last_measurement_of_power($phase_no),JSON_NUMERIC_CHECK);
+    echo  "{".'"data"'.":"." ".$result."}";
+
+}else if($cmd==1){
+
+    $result = json_encode($ems_db->last_measurement_of_current($phase_no),JSON_NUMERIC_CHECK);
+    echo  "{".'"data"'.":"." ".$result."}";
+
+}
+
