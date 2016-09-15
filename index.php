ffcf0d89095c4f6d411de920bb59100e27b3b697
Majkel44 <m.werner9205@gmail.com>
2016-09-15 21:30:00 +0200

    - added js function to displaying google gauges

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include 'GUI\Navbar.php';
 <html>
 
 <head>
-    <script type="text/javascript" src ="mysql_base/db_functions.js"></script>
+
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
@@ -26,12 +26,80 @@ include 'GUI\Navbar.php';
     <link rel="stylesheet" href="darkly/theme/usebootstrap.css">
     <script src="bootstrap/html5shiv.js"></script>
     <script src="bootstrap/respond.min.js"></script>
+
+    <script type="text/javascript">
+        google.load("visualization", "1", {packages:["gauge"]});
+        google.setOnLoadCallback(function(){
+
+            drawChart("L1","controllers/EMS_Controller.php?cmd=0&phase_no=1","l1_div");
+            
+
+
+        });
+
+
+        function drawChart(label,url,div_id) {
+
+            var chart_data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                [label, 0],
+
+
+            ]);
+
+
+            var options = {
+                width: 400, height: 400,
+                redFrom: 90, redTo: 100,
+                yellowFrom:75, yellowTo: 90,
+                minorTicks: 5
+            };
+
+            var chart = new google.visualization.Gauge(document.getElementById(div_id));
+
+
+            chart.draw(chart_data, options);
+
+
+
+            setInterval(function() {
+
+                $.getJSON(url,function(data) {
+
+
+
+                    $.each(data.data, function(index,value){
+
+                        
+
+                        chart_data.setValue(0,1,value.data);
+                        chart.draw(chart_data,options);
+
+                    });
+
+
+
+                });
+
+
+
+            },1000);
+
+        }
+    </script>
+
 </head>
 
 
 <body>
 <h2> Internetowy System Monitorowania Instalacji </h2>
 <?php $nav = new Navbar("index.php",0);?>
+
+    <div class="container">
+
+        <div id = "l1_div">  </div>
+
+    </div>
 
 </body>
 
