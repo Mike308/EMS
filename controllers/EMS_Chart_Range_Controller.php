3d921b5385b1cda674ad20a6d49a4a01c2be0779
Majkel44 <m.werner9205@gmail.com>
2016-09-17 14:55:32 +0200

    - added controller for preparing json format data of current and power of range

---
+++ controllers/EMS_Chart_Range_Controller.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 2016-09-17
+ * Time: 14:20
+ */
+
+
+
+
+include "../services/EMS_DB.php";
+include "../config/config.php";
+
+
+$cmd = $_GET['cmd'];
+$start = $_GET['start'];
+$end = $_GET['end'];
+
+
+
+
+$ems_db = new EMS_DB(user_db,password,localhost);
+$ems_db->open_db();
+
+
+
+
+
+if($cmd==1){
+
+    $result = json_encode($ems_db->prepare_measurement_of_power_from_range($start,$end),JSON_NUMERIC_CHECK);
+     echo $result;
+
+}else if($cmd==2){
+
+    $result = json_encode($ems_db->prepare_measurement_of_power_from_range($start,$end),JSON_NUMERIC_CHECK);
+    echo $result;
+
+
+}
+
+
