9fa2ed5803eaca273edba24131fa57eac54af982
Majkel44 <m.werner9205@gmail.com>
2016-10-06 20:36:35 +0200

    added md5 convert to password

--- controllers/EMS_Auth_Controller.php
+++ controllers/EMS_Auth_Controller.php
@@ -11,13 +11,13 @@ include "../services/Auth_Gate.php";
 
 
 $login = $_POST['login'];
-$password = $_POST['password'];
+$password = md5($_POST['password']);
 
 
 
 $auth = new Auth_Gate();
 $auth->start_session();
-header("Location: http://".localhost."/EMS2/index.php");
+
 $auth->check_login($login,$password);
 
 
